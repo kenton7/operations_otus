cee455856c98f77669a87365fb9bd70c29cc202a
Ilya Kuznetsov <kuznecov95-95@yandex.ru>
2023-12-27 20:51:07 +0300

    operationQueue

---
+++ Operation_ДЗ/GetImage.swift
@@ -0,0 +1,67 @@
+//
+//  GetImage.swift
+//  Operation_ДЗ
+//
+//  Created by Илья Кузнецов on 27.12.2023.
+//
+
+import Foundation
+import UIKit
+
+class GetImage: Operation {
+    
+    static let shared = GetImage()
+    
+    private override init() {}
+    
+    private let imageURLs = [
+        "https://www.nastol.com.ua/pic/201509/1920x1200/nastol.com.ua-150653.jpg",
+        "https://onn.az/wp-content/uploads/2021/06/sagzh-1920x1280-1.jpg",
+        "https://static.slobodnadalmacija.hr/images/slike/2023/01/12/24127418.jpg",
+        "https://content.assets.pressassociation.io/2017/07/18153241/17_7_5_WildcatKitten_SA_11.jpg",
+        "https://www.cestujlevne.com/obrazky/70/12/17012-2160w.jpg",
+        "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663091157_62-mykaleidoscope-ru-p-gollandiya-vkontakte-68.jpg",
+        "https://bonpic.com/download_img.php?dimg=5286&raz=1280x1024"
+    ]
+    
+    var completion: ((UIImage) -> Void)?
+    let operationQueue = OperationQueue()
+    let urlCache = URLCache.shared
+    
+    override func main() {
+        let sessionConfig = URLSessionConfiguration.default
+        let urlSession = URLSession(configuration: sessionConfig)
+        operationQueue.qualityOfService = .userInitiated
+        operationQueue.isSuspended = true
+        operationQueue.maxConcurrentOperationCount = imageURLs.count
+        
+        for url in imageURLs {
+            guard let safeUrl = URL(string: url) else { continue }
+            
+            let request = URLRequest(url: safeUrl)
+            
+            if let cachedData = self.urlCache.cachedResponse(for: request)?.data, let cachedImage = UIImage(data: cachedData) {
+                print("cached")
+                self.completion?(cachedImage)
+            } else {
+                operationQueue.addOperation {
+                    urlSession.dataTask(with: request) { data, response, error in
+                        if let error = error {
+                            print("Error: \(error)")
+                            return
+                        }
+                        
+                        if let data = data, let response = response, let image = UIImage(data: data) {
+                            let cachedData = CachedURLResponse(response: response, data: data)
+                            self.urlCache.storeCachedResponse(cachedData, for: request)
+                            print("no cache")
+                            self.completion?(image)
+                        }
+                    }.resume()
+                }
+                operationQueue.isSuspended = false
+                operationQueue.waitUntilAllOperationsAreFinished()
+            }
+        }
+    }
+}
--- Operation_ДЗ/ViewController.swift
+++ Operation_ДЗ/ViewController.swift
@@ -9,11 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var imagesArray = [UIImage]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(tableView)
+        //operationQueue()
+        
+        GetImage.shared.completion = { image in
+            self.imagesArray.append(image)
+        }
+        GetImage.shared.start()
     }
+    
+//    private func operationQueue() {
+//        let getImage = GetImage()
+//        getImage.queuePriority = .veryHigh
+//        getImage.qualityOfService = .userInitiated
+//        getImage.completion = { image in
+//            self.imagesArray.append(image)
+//            print(self.imagesArray.count)
+//        }
+//        getImage.start()
+//    }
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imagesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.selectionStyle = .none
+        cell.imageView?.image = imagesArray[indexPath.row]
+        return cell
+    }
+    
 }
 
